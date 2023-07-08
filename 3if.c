5b8771b3704e2907e3dc0be3f8bff1cb3d7e857f
StellaMn03 <138888432+StellaMn03@users.noreply.github.com>
2023-07-08 23:22:28 +0400

    Add files via upload

---
+++ 3if.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+int num=0;
+int a=0;
+int b=0;
+int c=0;
+scanf("%d",&num);
+if(num>77)
+   a=num/2,b=num*2,c=num-1;
+   printf("%d %d %d",a,b,c);
+   return 0;
+
+}
