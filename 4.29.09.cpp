c1abd2c64cd21d8bc470c7f3e8d0802138e0d37d
StellaMn03 <138888432+StellaMn03@users.noreply.github.com>
2023-09-30 12:05:44 +0400

    Add files via upload

---
+++ 4.29.09.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+int main(){
+    const int n = 3;
+	int arr[n];
+	int** ptr = (int**)malloc(n*sizeof(int*));
+	for(int i = 0;i < n;++i)
+	{
+			ptr[i]=(int*)malloc(n*sizeof(int));
+	}
+	for(int i = 0;i < n;i++)
+	{
+		for(int j = 0;j < n;j++)
+		{
+			std::cin >> ptr[i][j];
+		}
+	}
+int sum = 0;
+for(int i = 0;i<n/2*1.0;i++){
+	for(int j = i;j<n-i;j++){
+		sum+=ptr[i][j];
+	}
+}
+std::cout << sum;
+	for(int i = 0;i < n;++i){
+		free(ptr[i]);
+	}
+	free(ptr);	return 0;
+	}
+
